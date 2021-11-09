30e90411cb9d863b2562690a6e65730603988472
Luziane Freitas <freitas.lu@outlook.com>
2021-11-08 23:59:49 -0300

    adicionado sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1>Hello ignews</h1>
+    <h1 className={styles.title} >Hello ignews</h1>
   )
 }
