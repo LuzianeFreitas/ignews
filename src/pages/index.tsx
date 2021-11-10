526e5053695c6e268a2bbb5ae99440295d8fc167
Luziane Freitas <freitas.lu@outlook.com>
2021-11-09 23:38:25 -0300

    adicionado estilo global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/home.module.scss'
+
 
 export default function Home() {
   return (
-    <h1 className={styles.title} >Hello ignews</h1>
+    <>
+      <Head>
+        <title>Ig.news</title>
+      </Head>
+      <h1>
+
+        Hello ignews
+      </h1>
+    </>
   )
 }
