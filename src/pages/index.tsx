1b3d73adb664fdc98d080bc4eabfb9c0e4252ff8
Luziane Freitas <freitas.lu@outlook.com>
2021-12-14 20:50:43 -0300

    adicionado estrutura da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
 import Head from 'next/head';
-
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | Ig.news</title>
+        <title>Home | Ig.news</title>
       </Head>
-      <h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
 
-        Hello ignews
-      </h1>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
