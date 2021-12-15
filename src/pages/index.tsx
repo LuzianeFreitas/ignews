6ffde82caa0021e02924dd8dacae8f4150b23352
Luziane Freitas <freitas.lu@outlook.com>
2021-12-14 21:03:54 -0300

    adicionado componente subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -9,12 +10,13 @@ export default function Home() {
       </Head>
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>👏 Hey, welcome</span>
+          <span>👏</span><span> Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
