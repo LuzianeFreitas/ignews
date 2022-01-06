4c58a5840dffed63e5a80a5f3520dfa7fd78b018
Luziane Freitas <freitas.lu@outlook.com>
2022-01-05 22:27:19 -0300

    autenticação com github em andamento

--- src/components/SignInButton.tsx/index.tsx
+++ src/components/SignInButton.tsx/index.tsx
@@ -1,11 +1,15 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, useSession } from 'next-auth/react';
+
 import styles from './styles.module.scss';
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const {data:session} = useSession();
 
-    return isUserLoggedIn ? (
+    console.log(session);
+    
+    return session ? (
         <button 
             type="button"
             className={styles.signInButton}
@@ -18,6 +22,7 @@ export function SignInButton() {
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417"/>
             Sing in with GitHub
