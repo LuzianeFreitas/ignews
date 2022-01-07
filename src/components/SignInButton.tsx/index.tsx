dbac6b57f6d3bc6511f49c2419bf064b633b691c
Luziane Freitas <freitas.lu@outlook.com>
2022-01-06 21:35:01 -0300

    correção autenticação com github

--- src/components/SignInButton.tsx/index.tsx
+++ src/components/SignInButton.tsx/index.tsx
@@ -1,7 +1,7 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
-import { signIn, useSession } from 'next-auth/react';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 export function SignInButton() {
@@ -13,9 +13,10 @@ export function SignInButton() {
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361"/>
-            Luziane Freitas
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
