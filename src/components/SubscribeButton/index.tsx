b25d5053c78d11fbadc0f91a614efc250d854fb2
Luziane Freitas <freitas.lu@outlook.com>
2022-01-13 23:26:46 -0300

    criando sessão de checkout com stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 
@@ -5,10 +6,22 @@ interface SubscribeButtonProps {
     priceId: string;
 }
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    function handleSubscribe() {
+        // verifica se o usuário esta logado para poder inscrever
+        if(!session) {
+            signIn('github');
+            return;
+        }
+
+        
+    }
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { stripe } from "../../services/stripe";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if(req.method === 'POST') {
+        const  session = await getSession({ req })
+        
+        // criação do cliente no stripe
+        const stripeCustomer = await stripe.customers.create({
+            email: session.user.email,
+            // metadata
+        })
+
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: stripeCustomer.id,
+            payment_method_types: ['card'],
+            billing_address_collection: 'required',
+            line_items: [{
+                price: 'price_1K6lAZAj6mV2TbtnOKiE7BhA',
+                quantity: 1
+            }],
+            mode: 'subscription',
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL
+        })
+
+        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+    } else {
+       res.setHeader('Allow', 'POST')
+       res.status(405).end('Method not allowed')
+    }
+}
