2a429d1d2d814f4888e623a8801651ad50b15f54
Luziane Freitas <freitas.lu@outlook.com>
2022-01-17 21:34:36 -0300

    erro com session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { stripe } from '../../services/stripe';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 
@@ -7,15 +10,26 @@ interface SubscribeButtonProps {
 }
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
     const [session] = useSession();
-
-    function handleSubscribe() {
+    console.log(session);
+    
+    async function handleSubscribe() {
         // verifica se o usuário esta logado para poder inscrever
         if(!session) {
             signIn('github');
             return;
         }
 
-        
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs()
+            
+            await stripe.redirectToCheckout(sessionId)
+        } catch (err) {
+            alert(err.message)
+        }
     }
     return (
         <button
---
+++ src/services/stripe-js.ts
@@ -0,0 +1,9 @@
+// integração do stripe com o browser
+import { loadStripe } from "@stripe/stripe-js";
+
+
+export async function getStripeJs() {
+    const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
+
+    return stripeJs;
+}
