a32b344d2939e538bb024345d1eb5fbeb5021bcf
Luziane Freitas <freitas.lu@outlook.com>
2022-04-25 22:45:33 -0300

    adicionado activeLink

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,8 +1,24 @@
-import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
+import Link, { LinkProps } from 'next/link';
+import { ReactElement, cloneElement } from 'react';
 
-export function ActiveLink() {
+interface AcriveLinkProps extends LinkProps {
+    children: ReactElement;
+    activeClassName: string;
+}
+
+export function ActiveLink({ children, activeClassName, ...rest }: AcriveLinkProps) {
+    const { asPath } = useRouter();
+    
+    const className = asPath === rest.href
+        ? activeClassName
+        : '';
+
     return (
-        <Link>
+        <Link {...rest}>
+            {cloneElement(children, {
+                className,
+            })}
         </Link>
     );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,19 @@
-import Link from 'next/link';
 import styles from './styles.module.scss';
 import { SignInButton } from '../SignInButton.tsx';
-import { useRouter } from 'next/dist/client/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
-    const { asPath } = useRouter();
-
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <Link href="/">
-                        <a  className={asPath === '/' ? styles.active : ''}>Home</a>
-                    </Link>
-                    <Link href="posts">
-                        <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
-                    </Link>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton/>
